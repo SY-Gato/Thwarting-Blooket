618e3a9f4463e390ae095704c039a95eb1f3bef6
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-10-11 12:16:42 -0500

    Update MainBookmark.js

--- MainBookmark.js
+++ MainBookmark.js
@@ -3,4 +3,4 @@ fetch("https://raw.githubusercontent.com/SY-Gato/Thwarting-Blooket/refs/heads/ma
   .then(res => res.text()
         .then(txt => eval(txt)));
 */
-fetch("https://raw.githubusercontent.com/SY-Gato/Thwarting-Blooket/refs/heads/main/GetToken.js").then(res => res.text().then(txt => eval(txt)));
+javascript:fetch("https://raw.githubusercontent.com/SY-Gato/Thwarting-Blooket/refs/heads/main/GetToken.js").then(res => res.text().then(txt => eval(txt)));
